2edca3a5805f5ae56b94b40c6ee94bb36a7ff254
Jordan D'Souza <121935089+jordan-dsouza@users.noreply.github.com>
2025-12-15 18:10:27 +0530

    Delete Task2 directory

--- Task2/app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Fynd AI Feedback System")
-
-st.title("Fynd AI Feedback System")
-st.write("Use the sidebar to navigate between dashboards.")
